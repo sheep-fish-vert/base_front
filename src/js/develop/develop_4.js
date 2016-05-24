9a066d9ca717b9161b70bca2d451a53709d3c1aa
radiks <radion91@gmail.com>
2016-05-24 11:20:09 +0300

    p1b1

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,8 +1,15 @@
-
+function mainPageSlider(){
+    $('.index_block_1_slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+}
 
 
 $(document).ready(function(){
-
+    mainPageSlider();
 });
 
 $(window).load(function(){
