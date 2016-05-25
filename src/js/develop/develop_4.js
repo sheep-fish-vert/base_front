29a4f4868dc192c9a880d6fb74a8f6e8aa4e41e0
radiks <radion91@gmail.com>
2016-05-25 10:36:12 +0300

    calculator

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -7,8 +7,32 @@ function mainPageSlider(){
     });
 }
 
+function numberWithSpaces(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    return parts.join(".");
+}
+
+
+function miniCalculator(){
+    var firstI = $('#first_input');
+    var lastI = $('#last_input');
+
+    firstI.keyup(function(event) {
+        var that = $(this);
+        lastI.val( Math.ceil( parseFloat( that.val()*coefCalculator ) ) );
+    });
+    lastI.keyup(function(event) {
+        var that = $(this);
+        firstI.val( Math.ceil( parseFloat( that.val()/coefCalculator ) ) );
+    });
+}
+
+
+
 
 $(document).ready(function(){
+    miniCalculator();
     mainPageSlider();
 });
 
